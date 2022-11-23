ac80cddea88f64d1d29e6f2c4834fd7aac4b7f8c
codazoda <joel@joeldare.com>
2022-11-23 07:47:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Boiler
+
+Boiler is an application that automatically prints basic boiler plate code that you can copy and paste into your project.
+
+:Screenshot Goes Here:
+
+Boiler downloads it's templates from a public website and uses GitHub for contributing to the project. In a future version you'll be able to use your own URL as an additonal template repository.
+
+
+## Examples
+
+```
+$boiler go
+
+$boiler go/http
+
+$boiler go/https
+```
+
+
+## Roadmap
+
+
+### Adding a Path
+
+You can create your own templates by putting them online then adding a boiler environment variable.
+
+boiler=https://example.com/boiler
---
+++ boiler.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	templateDir := "~/broiler/template/"
+	if os.Getenv("BOILER") != "" {
+		templateDir = os.Getenv("BOILER")
+	}
+	var templateFile string
+	// TODO: Use an environment variable as the template directory
+	// Make sure there is only one command line argument
+	argLength := len(os.Args[1:])
+	if argLength != 1 {
+		showSyntax()
+	}
+	// Grab the first command line argument
+	template := os.Args[1]
+	// Check if a main file exists for this template name
+	templateFile = templateDir + template + "/main.txt"
+	_, err := os.Stat(templateFile)
+	if os.IsNotExist(err) {
+		// Check if a matching file exists for this template name
+		templateFile = templateDir + template + ".txt"
+		_, err := os.Stat(templateFile)
+		if os.IsNotExist(err) {
+			fmt.Printf("Error: Template file not found (%s)\n", templateFile)
+			os.Exit(1) // TODO: Fix exit code
+		}
+	}
+	// TODO: Output the file
+	cat(templateFile)
+}
+
+// Cat a file to stdout
+func cat(fname string) {
+	fh, err := os.Open(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = io.Copy(os.Stdout, fh)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Output the program syntax
+func showSyntax() {
+	fmt.Println("Broiler v1.0.0")
+	fmt.Println("Syntax: boiler [template]")
+	os.Exit(1)
+}
